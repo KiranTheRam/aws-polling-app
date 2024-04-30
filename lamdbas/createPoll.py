2b0ec0b585872cd67e64a2286d17c3c9215f614b
KiranTheRam <kramji12@gmail.com>
2024-04-29 20:27:39 -0400

    Update createPoll.py
    added validation

--- lamdbas/createPoll.py
+++ lamdbas/createPoll.py
@@ -10,6 +10,21 @@ def lambda_handler(event, context):
     # Parse the incoming request body
     body = json.loads(event['body'])
     
+    # Validate that the data is not empty
+    if not body.get('question') or not body.get('options'):
+        response = {
+            'statusCode': 400,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',  # Allow requests from any origin
+                'Access-Control-Allow-Headers': 'Content-Type',  # Allow only Content-Type header
+                'Access-Control-Allow-Methods': 'OPTIONS, POST'  # Allow OPTIONS and POST methods
+            },
+            'body': json.dumps({
+                'error': 'Question and options cannot be empty'
+            })
+        }
+        return response
+
     # Generate a unique ID for the new poll
     poll_id = str(uuid.uuid4())
     
@@ -57,4 +72,3 @@ def lambda_handler(event, context):
             })
         }
         return response
-
